8d13c13453a1fc0b3a74c538b7df178475ffbcf9
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-06 08:04:16 +0100

    Create Motion field techniques.h

---
+++ Motion field techniques.h
@@ -0,0 +1,13 @@
+//if we use equations here, we will be able
+//to ttack ai outputs. but techniques involves in pure algorithms sometimes.
+//then lets create basic behaviours zgain.
+
+//this is not a model
+class basicbehaviourDintPatch
+
+public:
+  void checkAlternativeWaysToComminicate(..)
+{
+   ...
+   return hiddenlayer; 
+}
