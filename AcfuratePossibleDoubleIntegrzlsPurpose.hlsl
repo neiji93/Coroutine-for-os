7816648e6eee63e8544df749b9a67f6fd4a73c23
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-06 05:39:08 +0100

    Update AcfuratePossibleDoubleIntegrzlsPurpose.hlsl

--- AcfuratePossibleDoubleIntegrzlsPurpose.hlsl
+++ AcfuratePossibleDoubleIntegrzlsPurpose.hlsl
@@ -1,1 +1,10 @@
 // taken from france mpsi
+
+//the behavuour of a... system depends on duration
+//of the datas. so let s intriduce common tasks. but to enhance this feature,
+//we can use ai with patterns of a critical message !
+
+void criticalMessageGrantAccessToAllWithAccurateTimelapse(....)
+{}
+
+//lets manage messages which try to communicate with ips
