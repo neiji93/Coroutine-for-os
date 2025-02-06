f2be344d6eb3b6bad0d17bf3c33caa72cbaf46cc
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-06 18:02:13 +0100

    Create CarlrmanMethodComparedToAnySolverIntepretor.h

---
+++ CarlrmanMethodComparedToAnySolverIntepretor.h
@@ -0,0 +1,14 @@
+//maybe we can use hichin algorithm
+//but to ttansform into physics, we can reject access
+//to wide range of network datas if wr add coroutines sleep.
+//a muted algorithm can be good iff we drop some terms.
+//so no need of quantifiers
+
+class quantifierLowrangeMessages{}
+
+//if it inerits from some classes
+//we will not need perfect matching.
+class schedullermain
+{
+
+}
